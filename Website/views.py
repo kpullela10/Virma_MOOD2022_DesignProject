0df7fe611822e5d8d7f0461acbd2187e7f617e40
kpullela10 <kpullela10@gmail.com>
2022-05-02 21:16:09 -0700

    Added files and imported flask

---
+++ Website/views.py
@@ -0,0 +1,4 @@
+from flask import Flask
+
+def create_app(__name__):
+    #code goes here, stopped at 9:28 https://www.youtube.com/watch?v=dam0GPOAvVI&list=WL&index=1&t=567s
--- test.py
+++
@@ -1,1 +0,0 @@
-hefwjknl g
