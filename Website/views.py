efc6bd7213641414a8939261215fc7a89535271a
kpullela10 <kpullela10@gmail.com>
2022-05-03 12:14:59 -0700

    Finished up until 21:50 https://www.youtube.com/watch?v=dam0GPOAvVI&list=WL&index=1&t=568s

--- Website/views.py
+++ Website/views.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Blueprint
 
-def create_app(__name__):
-    #code goes here, stopped at 9:28 https://www.youtube.com/watch?v=dam0GPOAvVI&list=WL&index=1&t=567s
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def home():
+    return '<h1>test</h1>'
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Website import create_app
 
 app = create_app()
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     app.run(debug=True)
 
