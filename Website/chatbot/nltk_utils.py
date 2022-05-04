d95fdedfd85e39425ef9ffc463dad5ca430125e3
kpullela10 <kpullela10@gmail.com>
2022-05-04 15:01:39 -0700

    reformat

--- Website/chatbot/nltk_utils.py
+++ Website/chatbot/nltk_utils.py
@@ -1,13 +1,20 @@
 import nltk
+
 nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
+
 stemmer = PorterStemmer()
+
+
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
+
 
 def stem(word):
     return stemmer.stem(word.lower())
 
-def bag_of_words(tokenized_sentence ,all_words):
+
+def bag_of_words(tokenized_sentence, all_words):
     pass
+
 
